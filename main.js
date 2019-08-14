df14bb44f58a96a0f268a900a82ce133f1daa7de
Lihzahrd <47698047+Lihzahrd@users.noreply.github.com>
2019-08-14 15:21:57 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+var endpoint = "https://www.jsonstore.io/069bdffc396d93d0f2def86243bad9a83d230242150a00454420629c533e30ce"
+
+function getrandom(){    var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);    return random_string()}
